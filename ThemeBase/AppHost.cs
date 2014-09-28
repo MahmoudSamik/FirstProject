abcb66e719b3c0734e839fa85e0c3109e8bf7617
mahmoud <mahmoud-sami@hotmail.com>
2014-09-28 17:19:14 +0300

    first commit

---
+++ ThemeBase/AppHost.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using ServiceApi;
+using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using ServiceStack.Razor;
+using ServiceStack.OrmLite.SqlServer;
+
+namespace ThemeBase
+{
+    public class AppHost :AppHostBase 
+    {
+        public AppHost()
+            : base("ThemeBase", typeof(TestService).Assembly)
+        {
+            
+        }
+        public override void Configure(Funq.Container container)
+        {
+            Plugins.Add(new RazorFormat());
+
+            SetConfig(new HostConfig { AppendUtf8CharsetOnContentTypes = new HashSet<string> { MimeTypes.Html }, AllowJsonpRequests = true, AllowFileExtensions = { "json" } });
+
+            var conString = ConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString;
+            var conFactory = new OrmLiteConnectionFactory(conString, SqlServerOrmLiteDialectProvider.Instance, true);
+            container.Register<IDbConnectionFactory>(c => conFactory);
+
+       
+          
+
+        }
+    }
+}
---
+++ ThemeBase/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using ServiceStack.MiniProfiler;
+
+namespace ThemeBase
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+
+        
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Request.IsLocal)
+            {
+                Profiler.Start();
+            }
+        }
+       
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Profiler.Stop();
+        }
+    }
+}
